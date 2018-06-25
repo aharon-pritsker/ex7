9482bea60f1f2354d4ac65e8ad9c6e571675b483
Adi <adios12@gmail.com>
2018-06-25 13:46:41 +0300

    askfja

---
+++ src/MethodScope.java
@@ -0,0 +1,10 @@
+public class MethodScope extends Scope {
+
+    boolean returnFlag;
+    public MethodScope(int depth, Scope parent){
+        super(depth, parent);
+        returnFlag = false;
+    }
+
+    public 
+}
---
+++ src/Scope.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+
+public class Scope {
+
+    Scope childScope;
+    int scopeDepth;
+    boolean closed;
+    ArrayList<Variable> scopeVariables;
+
+    /**
+     * This is the scope's constructor
+     * @param depth the scope's depth
+     * @param parent the scope's parent
+     */
+    public Scope(int depth, Scope parent) {
+        scopeDepth = depth;
+        scopeVariables = new ArrayList<Variable>();
+        childScope = null;
+        Scope parentSCope = parent;
+        closed = false;
+    }
+
+    /**
+     *
+     * @return the depth of the scope
+     */
+    public int getDepth() {
+        return scopeDepth;
+    }
+
+    /**
+     * This method sets a child scope to the given one
+     * @param childScope the child scope
+     */
+    public void setChildScope(Scope childScope) {
+        this.childScope = childScope;
+    }
+
+    /**
+     * This method finds the index of a certain variable in the variables list
+     * @param varName the variable's name
+     * @return the variable's index if it exists in the list, -1 if it doesn't
+     */
+    private int findVarInList(String varName) {
+        for (Variable var: scopeVariables) {
+            if(var.getName().equals(varName)) {
+                return scopeVariables.indexOf(var);
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * This method add a new variable to the scope
+     * @param newVar the new variable
+     */
+    public void addVariable(Variable newVar) {
+        int varIndex = findVarInList(newVar.getName());
+        if(varIndex != -1) {
+            scopeVariables.remove(varIndex);
+        }
+        scopeVariables.add(newVar);
+    }
+
+    /**
+     * This method closes the current scope.
+     */
+    public void closeScope() {
+        closed = true;
+    }
+
+}

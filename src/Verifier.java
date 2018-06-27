b9644f00b7aad584876429c2b42b326f3a0c9d95
aharon-pritsker <aharonpritsker@gmail.com>
2018-06-28 00:04:26 +0300

    Merge branch 'master' of https://github.com/aharon-pritsker/ex7
    # Conflicts:
    #	src/Parser.java

--- src/Verifier.java
+++
@@ -1,9 +0,0 @@
-import java.util.regex.Pattern;
-
-public abstract class Verifier {
-
-    protected static final Pattern LegalName = Pattern.compile("^_\\w+|[A-Za-z]\\w*");
-    protected static String verifiedName;
-
-    abstract boolean verify(String toCheck);
-}
